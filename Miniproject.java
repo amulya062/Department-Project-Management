1ba10ae40991ec4fc2744cc5212d28a6e6d01c96
AMULYA S S <142977216+amulya062@users.noreply.github.com>
2025-03-20 22:58:58 +0530

    Add files via upload

---
+++ Miniproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miniproject;
+
+/**
+ *
+ * @author ANAGHA
+ */
+public class Miniproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
